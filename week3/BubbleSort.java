b42244d61c61d7c73547e174b3c4e9e08829754f
AnhDung727277 <anhdung7205@gmail.com>
2025-02-11 10:15:03 +0700

    Add files via upload

---
+++ week3/BubbleSort.java
@@ -0,0 +1,43 @@
+package week3;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class BubbleSort { 
+    public static void performBubbleSort() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int[] arr = generateRandomArray(n);
+
+        System.out.println("Unsorted Array: " + Arrays.toString(arr));
+        bubbleSort(arr);
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
+    }
+
+    private static int[] generateRandomArray(int size) {
+        Random random = new Random();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = random.nextInt(100); 
+        }
+        return arr;
+    }
+
+    private static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+        do {
+            swapped = false;
+            for (int i = 1; i < n; i++) {
+                if (arr[i - 1] > arr[i]) {
+                    int temp = arr[i - 1];
+                    arr[i - 1] = arr[i];
+                    arr[i] = temp;
+                    swapped = true;
+                }
+            }
+        } while (swapped);
+    }
+}
